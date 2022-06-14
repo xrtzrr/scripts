8916e7075c73d294553cd69501e1daedeacc2cff
LoopyPawss <101439821+LoopyPawss@users.noreply.github.com>
2022-06-14 16:36:02 -0600

    Update MURDER.lua

--- MURDER.lua
+++ MURDER.lua
@@ -1,5 +1,10 @@
+print("Script made by LoopyPawss.")
+print("For the game: MURDER.")
+print("Game ID: 4572253581")
+print("Made for the current version: 6/14/22")
+print("Enjoy exploiting.")
 --[[---------------------------------------------------------------------------
-Give the script 5-10 seconds to load in order to work.
+Give the script 5-10 seconds to load in order to work, please!
 GAME: [ https://www.roblox.com/games/4572253581/MURDER ]
 Special thanks to LoopyPawss for searching scripts for this dumb game!
 ███╗   ███╗██╗   ██╗██████╗ ██████╗ ███████╗██████╗ 
