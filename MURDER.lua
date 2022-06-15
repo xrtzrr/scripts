eeba94cc929441d8be0101d31dd976aebc25224a
LoopyPawss <101439821+LoopyPawss@users.noreply.github.com>
2022-06-14 20:48:33 -0600

    Update MURDER.lua

--- MURDER.lua
+++ MURDER.lua
@@ -1,10 +1,5 @@
-consoleprint("Script made by LoopyPawss.")
-consoleprint("For the game: MURDER.")
-consoleprint("Game ID: 4572253581")
-consoleprint("Made for the current version: 6/14/22")
-consoleprint("Enjoy exploiting.")
                                                                                                                                                          --[[
-Give the script 5-10 seconds to load in order to work, please!
+Please wait 10 seconds until the script is executed fully.
 GAME: [ https://www.roblox.com/games/4572253581/MURDER ]
 Special thanks to LoopyPawss for searching scripts for this dumb game!
  _____ ______   ___  ___  ________  ________  _______   ________     
@@ -16,16 +11,21 @@ Special thanks to LoopyPawss for searching scripts for this dumb game!
     \|__|     \|__|\|_______|\|__|\|__|\|_______|\|_______|\|__|\|__|
                                                                      
                                                                                                                                                          ]]
+consoleprint("Script made by LoopyPawss, credits to the owners of the scripts below the source code of the loadstring.")
+consoleprint("For the game: MURDER")
+consoleprint("Game ID: 4572253581")
+consoleprint("Made for the current version/Tested at: 6/14/22")
+consoleprint("Enjoy exploiting.")
 
---> ESP
+-- ESP
 loadstring(game:HttpGet("https://pastebin.com/raw/gpL2gJBg", true))()
 
 wait(5)
 
---> Infinite Yield
+-- Infinite Yield
 loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 
 wait(5)
 
---> Hitbox Expander
+-- Hitbox Expander
 loadstring(game:HttpGet("https://pastebin.com/raw/LgiSXqqF"))()
