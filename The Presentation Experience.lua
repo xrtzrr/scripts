5fa9bc26e08153fb765fa9e7ed1f2fb5eb749117
LoopyPawss <101439821+LoopyPawss@users.noreply.github.com>
2022-06-14 20:10:38 -0600

    Update The Presentation Experience.lua

--- The Presentation Experience.lua
+++ The Presentation Experience.lua
@@ -1,9 +1,9 @@
 print("Script made by LoopyPawss.")
 print("For the game: The Presentation Experience.")
 print("Game ID: 7772810845")
-print("Made for the current version/Tested at: 3/21/22")
+print("Made for the current version/Tested at: 6/14/22")
 print("Enjoy exploiting.")
---[[
+                                                                                                                                                         --[[
 Give the script 5 seconds to load in order to work, please!
 GAME: [ https://www.roblox.com/games/7772810845/TPE ]
 Special thanks to LoopyPawss for writing all the code for this game.
@@ -14,11 +14,11 @@ Special thanks to LoopyPawss for writing all the code for this game.
       \ \  \ \ \  \___|\ \  \_|\ \ 
        \ \__\ \ \__\    \ \_______\
         \|__|  \|__|     \|_______|
---]]
+                                                                                                                                                         --]]
 
 wait(5)
 
--- General Coding
+--// General Coding \\--
 
 -- Enables reset
 game:GetService("StarterGui"):SetCore("ResetButtonCallback", true)
@@ -37,28 +37,10 @@ game:GetService("ReplicatedStorage").RedeemCode:InvokeServer("210kmembers")
 game:GetService("ReplicatedStorage").RedeemCode:InvokeServer("220kmembers")
 game:GetService("ReplicatedStorage").RedeemCode:InvokeServer("push-ups")
 
+-- Allows your character to move around (MAY BE BUGGY!!!!)
+local char = game.Players.LocalPlayer.Character
+char.Sit:Destroy()
+char.Humanoid.WalkSpeed = 16
 
 -- Please wait 5 seconds until the script is executed fully.
 -- This is not finished. Expect bugs while executing if this script is old.
-
-
-
-
-
-
-
--- Coding below doesn't work, sorry
-
---[[
-
--- Allows your character to move around (MAY BE BUGGY!!!!)
-local plr = game.Players.LocalPlayer.Character
-
--- Removes seat
-plr.Sit:Destroy()
-wait()
-
--- Changes speed to normal
-game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
-
---]]
