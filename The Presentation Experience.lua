d05604253608575d5b0ae828daff6b1ada88bc5d
LoopyPawss <101439821+LoopyPawss@users.noreply.github.com>
2022-06-14 20:48:32 -0600

    Update The Presentation Experience.lua

--- The Presentation Experience.lua
+++ The Presentation Experience.lua
@@ -1,25 +1,21 @@
--- Please wait 5 seconds until the script is executed fully.
--- This is not finished. Expect bugs while executing if this script is 2-7 months/years old?
-
+                                                                                                                                                         --[[
+Please wait 5 seconds until the script is executed fully.
+GAME: [ https://www.roblox.com/games/7772810845/TPE ]
+Special thanks to LoopyPawss for coding the script!
+ _____ ______   ___  ___  ________  ________  _______   ________     
+|\   _ \  _   \|\  \|\  \|\   __  \|\   ___ \|\  ___ \ |\   __  \    
+\ \  \\\__\ \  \ \  \\\  \ \  \|\  \ \  \_|\ \ \   __/|\ \  \|\  \   
+ \ \  \\|__| \  \ \  \\\  \ \   _  _\ \  \ \\ \ \  \_|/_\ \   _  _\  
+  \ \  \    \ \  \ \  \\\  \ \  \\  \\ \  \_\\ \ \  \_|\ \ \  \\  \| 
+   \ \__\    \ \__\ \_______\ \__\\ _\\ \_______\ \_______\ \__\\ _\ 
+    \|__|     \|__|\|_______|\|__|\|__|\|_______|\|_______|\|__|\|__|
+                                                                     
+                                                                                                                                                         ]]
 consoleprint("Script made by LoopyPawss.")
-consoleprint("For the game: The Presentation Experience.")
+consoleprint("For the game: The Presentation Experience")
 consoleprint("Game ID: 7772810845")
 consoleprint("Made for the current version/Tested at: 6/14/22")
 consoleprint("Enjoy exploiting.")
-                                                                                                                                                         --[[
-Give the script 5 seconds to load in order to work, please!
-GAME: [ https://www.roblox.com/games/7772810845/TPE ]
-Special thanks to LoopyPawss for writing all the code for this game.
- _________  ________  _______      
-|\___   ___\\   __  \|\  ___ \     
-\|___ \  \_\ \  \|\  \ \   __/|    
-     \ \  \ \ \   ____\ \  \_|/__  
-      \ \  \ \ \  \___|\ \  \_|\ \ 
-       \ \__\ \ \__\    \ \_______\
-        \|__|  \|__|     \|_______|
-                                                                                                                                                         --]]
-
-wait(5)
 
 --// General Coding \\--
 
