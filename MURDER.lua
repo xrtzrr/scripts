52343ca0dfd81592330c9fbf97bd701ebf4bc3ae
LoopyPawss <101439821+LoopyPawss@users.noreply.github.com>
2022-06-16 19:25:37 -0600

    Update MURDER.lua

--- MURDER.lua
+++ MURDER.lua
@@ -11,7 +11,7 @@ Special thanks to LoopyPawss for searching scripts for this dumb game!
     \|__|     \|__|\|_______|\|__|\|__|\|_______|\|_______|\|__|\|__|
                                                                      
                                                                                                                                                                         ]]
-print("Script coded by owners of the IY, ESP, AND Hitbox Expander for the game MURDER with the game ID of 4572253581, made/tested at 6/14/22 by LoopyPawss")
+rconsoleprint("Script coded by owners of the IY, ESP, AND Hitbox Expander for the game MURDER with the game ID of 4572253581, made/tested at 6/14/22 by LoopyPawss, feel free to minimize this cmd prompt so you can start exploiting, enjoy!")
 
 -- ESP
 loadstring(game:HttpGet("https://pastebin.com/raw/gpL2gJBg", true))()
