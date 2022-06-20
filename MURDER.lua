83747564ea5a5ed5c0a647feb459f6c9896482eb
LoopyPawss <101439821+LoopyPawss@users.noreply.github.com>
2022-06-19 23:44:00 -0600

    Update MURDER.lua

--- MURDER.lua
+++ MURDER.lua
@@ -1,7 +1,7 @@
                                                                                                                                                                       --[[
 Please wait 10 seconds until the script is executed fully.
 GAME: [ https://www.roblox.com/games/4572253581/MURDER ]
-Special thanks to LoopyPawss for searching scripts for this dumb game!
+Special thanks to LoopyPawss for searching scripts for this game!
  _____ ______   ___  ___  ________  ________  _______   ________     
 |\   _ \  _   \|\  \|\  \|\   __  \|\   ___ \|\  ___ \ |\   __  \    
 \ \  \\\__\ \  \ \  \\\  \ \  \|\  \ \  \_|\ \ \   __/|\ \  \|\  \   
@@ -11,7 +11,12 @@ Special thanks to LoopyPawss for searching scripts for this dumb game!
     \|__|     \|__|\|_______|\|__|\|__|\|_______|\|_______|\|__|\|__|
                                                                      
                                                                                                                                                                         ]]
-rconsoleprint("Script coded by owners of the IY, ESP, AND Hitbox Expander for the game MURDER with the game ID of 4572253581, made/tested at 6/14/22 by LoopyPawss, feel free to minimize this cmd prompt so you can start exploiting, enjoy!")
+rconsolename("Credits")
+rconsoleprint("Script coded by owners of the IY, ESP, AND Hitbox Expander1, made/tested at 6/14/22 by LoopyPawss")
+rconsoleprint("Game: MURDER, GameID: 457225358")
+wait(30)
+rconsoleclear()
+rconsoleprint("You can now minimize this prompt. Enjoy exploiting!")
 
 -- ESP
 loadstring(game:HttpGet("https://pastebin.com/raw/gpL2gJBg", true))()
