23991c0086fafbb6cac3dcc43104ca9c521e001f
LoopyPawss <101439821+LoopyPawss@users.noreply.github.com>
2022-06-25 00:39:29 -0600

    Update ChatCrasher.lua

--- ChatCrasher.lua
+++ ChatCrasher.lua
@@ -192,3 +192,21 @@ local function GJTHJOF_fake_script() -- Frame.FrameDrag
 end
 
 coroutine.wrap(GJTHJOF_fake_script)()
+
+
+wait(2)
+-- Anti Report Ban just incase
+local CheckCaller = clonefunction(checkcaller)
+local HookFunction = clonefunction(hookfunction)
+local LocalPlayer = game:GetService("Players").LocalPlayer or game:GetService("Players").PlayerAdded:Wait()
+local PostMessage = require(LocalPlayer:WaitForChild("PlayerScripts", 1/0):WaitForChild("ChatScript", 1/0):WaitForChild("ChatMain", 1/0)).MessagePosted
+getgenv().MessageEvent = Instance.new("BindableEvent")
+local OldFunctionHook
+local PostMessageHook = function(self, msg)
+   if not CheckCaller() and self == PostMessage then
+       MessageEvent:Fire(msg)
+       return
+   end
+   return OldFunctionHook(self, msg)
+end
+OldFunctionHook = HookFunction(PostMessage.fire, PostMessageHook)
