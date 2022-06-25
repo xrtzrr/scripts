5018a1d5e1035920c99da99107b2816cdd385b35
LoopyPawss <101439821+LoopyPawss@users.noreply.github.com>
2022-06-25 00:07:17 -0600

    Create LoopWalkspeedChanger.lua

---
+++ LoopWalkspeedChanger.lua
@@ -0,0 +1,26 @@
+--This is like your normal walkspeed script, but doesn't reset when you reset. Enjoy. Made by LoopyPawss
+_G.Speed = 200 --Walkspeed, feel free to change.
+
+-- Locals
+local plrserv = game:GetService("Players")
+local plr = plrserv.LocalPlayer
+local chr = plr.Character
+ 
+if chr then
+local hum = chr.Humanoid
+hum.Changed:connect(function()
+hum.WalkSpeed = _G.Speed
+end)
+hum.WalkSpeed = _G.Speed
+end
+ 
+ 
+plr.CharacterAdded:connect(function(chr2)
+chr = chr2
+repeat wait() until chr:FindFirstChild("Humanoid")
+local hum2 = chr2.Humanoid
+hum2.Changed:connect(function()
+hum2.WalkSpeed = _G.Speed
+end)
+hum2.WalkSpeed = _G.Speed
+end)
