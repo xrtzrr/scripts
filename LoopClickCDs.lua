bce950a01b33495b283da5164dbb06d4a0684223
LoopyPawss <101439821+LoopyPawss@users.noreply.github.com>
2022-06-27 00:29:13 -0600

    Update LoopClickCDs.lua

--- LoopClickCDs.lua
+++ LoopClickCDs.lua
@@ -1,3 +1,4 @@
+--This script loopclicks all clickdetectors in-game and repeats after the local 'ttime' is finished
 --warn("Don't come to the github repos to edit the script, use this pastebin link instead: https://pastebin.com/raw/00000000")
 
 while true do
