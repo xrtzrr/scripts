a5418dd362220e7d61fdbb0f4b5d082894686773
LoopyPawss <101439821+LoopyPawss@users.noreply.github.com>
2022-06-27 00:27:59 -0600

    Create LoopClickCDs.lua

---
+++ LoopClickCDs.lua
@@ -0,0 +1,12 @@
+--warn("Don't come to the github repos to edit the script, use this pastebin link instead: https://pastebin.com/raw/00000000")
+
+while true do
+    repeat
+        wait(ttime)
+for i,v in pairs(workspace:GetDescendants()) do
+            if v:IsA("ClickDetector") then
+                fireclickdetector(v)
+            end
+        end
+until false
+end
