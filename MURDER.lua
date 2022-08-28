23feae7b550f135e12555f17760dbe4dd7bd4308
LoopyPawss <xDarkWasTaken001@gmail.com>
2022-08-28 16:38:00 -0600

    Update MURDER.lua

--- MURDER.lua
+++ MURDER.lua
@@ -1,24 +1,5 @@
-                                                                                                                                                                      --[[
-Please wait 10 seconds until the script is executed fully.
-GAME: [ https://www.roblox.com/games/4572253581/MURDER ]
-Special thanks to LoopyPawss for searching scripts for this game!
- _____ ______   ___  ___  ________  ________  _______   ________     
-|\   _ \  _   \|\  \|\  \|\   __  \|\   ___ \|\  ___ \ |\   __  \    
-\ \  \\\__\ \  \ \  \\\  \ \  \|\  \ \  \_|\ \ \   __/|\ \  \|\  \   
- \ \  \\|__| \  \ \  \\\  \ \   _  _\ \  \ \\ \ \  \_|/_\ \   _  _\  
-  \ \  \    \ \  \ \  \\\  \ \  \\  \\ \  \_\\ \ \  \_|\ \ \  \\  \| 
-   \ \__\    \ \__\ \_______\ \__\\ _\\ \_______\ \_______\ \__\\ _\ 
-    \|__|     \|__|\|_______|\|__|\|__|\|_______|\|_______|\|__|\|__|
-                                                                     
-                                                                                                                                                                        ]]
-rconsolename("Credits")
-wait(2)
-rconsoleprint("Script coded by owners of the IY, ESP, AND Hitbox Expander1, made/tested at 6/14/22 by LoopyPawss")
-wait(2)
-rconsoleprint("Game: MURDER, GameID: 457225358")
-wait(30)
-rconsoleclear()
-rconsoleprint("You can now minimize this prompt. Enjoy exploiting!")
+--loadstring(game:HttpGet('-https://raw.githubusercontent.com/LoopyPawss/scripts/main/MURDER.lua'))()
+--https://www.roblox.com/games/4572253581/MURDER
 
 -- ESP
 loadstring(game:HttpGet("https://pastebin.com/raw/gpL2gJBg", true))()
