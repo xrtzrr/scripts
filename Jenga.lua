9851fcc0e84974e0cba94822e6d4146986866af8
LoopyPawss <xDarkWasTaken001@gmail.com>
2022-08-29 20:20:54 -0600

    Update Jenga.lua

--- Jenga.lua
+++ Jenga.lua
@@ -15,46 +15,30 @@ local Credits = Other:NewSection("--/\/\ Other things to mention \/\/--")
 MainSection:NewButton("End game", "Wins the game if your ONLY on the Towers team.", function()
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-65.85745239257812, 99.9999771118164, -18.305282592773438)
 end)
-
 MainSection:NewButton("Remove killbricks", "Removes killbricks that reset your character.", function()
 game.Workspace.Map.Classic.KillBrick:Destroy()
 end)
-
 MainSection:NewButton("Remove objects", "Removes objects thrown by the Destroyer.", function()
 game.Workspace.Projectiles:Destroy()
 end)
-
 MainSection:NewButton("Fly", "Makes you fly.", function()
 loadstring(game:HttpGet(('https://pastebin.com/raw/WxmvCLLH'),true))()
 end)
-
 MainSection:NewButton("Reset", "Resets your character.", function()
 game.Players.LocalPlayer.Character["Head"]:Destroy()
 game.Players.LocalPlayer.Character["HumanoidRootPart"]:Destroy()
 end)
-
-Admins:NewButton("Infinite Yield", "Admin made by edge", function()
+Admins:NewButton("Infinite Yield", "Admin made by Edge", function()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 end)
-
 Admins:NewButton("Fates Admin", "Admin made by Fate", function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/fatesc/fates-admin/main/main.lua"))();
 end)
-
+MainSection:NewButton("Moai", "Send an moai emoji to make a loud boom noise.", function()
+loadstring(game:HttpGet("https://xn--9p9haaaaaa.tk/scripts/vine%20thud.lua"))()
+end)
 
 --// Credits
 Credits:NewLabel("UI Library was made by xHeptc on Github.")
-Credits:NewLabel("This will not get updated, sorry.")
-
-
-
-
---[[
-
-Extra stuff:
-
---// roblox.com/games/2537430692/1
---// https://emojipedia.org/moai/ <-- Copy this in chat. Makes a loud noise for everyone to hear.
-loadstring(game:HttpGet("https://xn--9p9haaaaaa.tk/scripts/vine%20thud.lua"))()
-
-]]
+Credits:NewLabel("Moai script made by skyr3x on V3rmillion.")
+Credits:NewLabel("This will never get updated, i'm sorry.")
