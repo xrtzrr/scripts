2c0d38ad4f8a35a8235346a35611905fc4483e0e
d7lta <xDarkWasTaken001@gmail.com>
2022-11-12 03:12:49 -0700

    Update IA_AdminKick.lua

--- IA_AdminKick.lua
+++ IA_AdminKick.lua
@@ -1,12 +1,10 @@
-                                                                  --[[
-
+--[[
 https://www.roblox.com/games/5670218884/IA
 loadstring(game:HttpGet('https://raw.githubusercontent.com/d7ltacat/scripts/main/IAKick.lua'))()
 Kicks you out of the game automatically if an item asylum admin/headadmin/developer joins in-game, can also kick you if a roblox QA tester or admin joins.
 The list will be updated if necessary, i'm not going to add contributors or testers, they won't do any good.
 Also, if the developers are reading this, fuck you for banning me, I did nothing wrong. So consider this payback.
-
-                                                                  --]]
+--]]
 
 if not game:IsLoaded() then
     game.Loaded:Wait()
@@ -40,6 +38,7 @@ Names = {
     "Xerroz"
 --  Count: 20 people in total
 }
+
 local function Check(Player)
     for _,GroupId in {
         1200769,
