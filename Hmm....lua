ff54b3301029955913b4326a5c6ec7220c27bb97
d7lta <xDarkWasTaken001@gmail.com>
2022-11-12 23:16:23 -0700

    Create Hmm....lua

---
+++ Hmm....lua
@@ -0,0 +1,3 @@
+--This scripts teleports you to the Obunga button, it is around the room in Hmm.... https://www.roblox.com/games/885702956/1
+chr=game.Players.LocalPlayer.Character
+chr.HumanoidRootPart.CFrame = CFrame.new(510, -5, 277)
