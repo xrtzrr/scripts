be3cfd4c70993b5947fb6bc3c8ad9afdf41a078e
d7lta <xDarkWasTaken001@gmail.com>
2022-11-12 23:17:05 -0700

    Update Hmm....lua

--- Hmm....lua
+++ Hmm....lua
@@ -1,3 +1,4 @@
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/d7ltacat/scripts/main/Hmm....lua"))()
 --This scripts teleports you to the Obunga button, it is around the room in Hmm.... https://www.roblox.com/games/885702956/1
 chr=game.Players.LocalPlayer.Character
 chr.HumanoidRootPart.CFrame = CFrame.new(510, -5, 277)
