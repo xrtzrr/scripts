a3161e3dc5300ebe5983dc7230281018623ab939
paw <5dkyQHivc7zYxjCmN4qAMH7qtkfPn8@proton.me>
2022-12-05 21:48:19 -0700

    Create DigToChina.lua

---
+++ DigToChina.lua
@@ -0,0 +1,14 @@
+loadstring(game:HttpGet('https://pastebin.com/raw/urvzBY4a'))()
+--Loadstring: loadstring(game:HttpGet(''))()
+--https://www.roblox.com/games/8506369721/dtc
+--Wait until timer hits 00:00 and you will get to China.
+local p=game.Players.LocalPlayer
+local h=plr.Character.HumanoidRootPart
+local c=workspace.ChinaDetector
+local f=firetouchinterest
+
+while true do
+   f(h, c, 0)
+   task.wait(.1)
+   f(h, c, 1)
+end
