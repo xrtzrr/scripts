10186cfe4c748bbed72b47f7c1252380a9ac2ec8
paw <5dkyQHivc7zYxjCmN4qAMH7qtkfPn8@proton.me>
2022-12-08 16:47:29 -0700

    A script for Murder that doesn't work that well.

--- MURDER.lua
+++ MURDER.lua
@@ -1,10 +1,9 @@
 --loadstring(game:HttpGet('https://raw.githubusercontent.com/pawstep/scripts/main/MURDER.lua'))()
 -- https://www.roblox.com/games/4572253581/MURDER
-
 -- ESP
 -- loadstring(game:HttpGet("https://pastebin.com/raw/00000000", true))()
-
 -- wait(5)
+
 
 -- Infinite Yield
 loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
