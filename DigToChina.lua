a051bdf3c5c4ea422094a8bd290e6629874bdcbc
paw <5dkyQHivc7zYxjCmN4qAMH7qtkfPn8@proton.me>
2022-12-08 16:41:11 -0700

    Auto-win for Dig to China.

--- DigToChina.lua
+++ DigToChina.lua
@@ -2,11 +2,11 @@ loadstring(game:HttpGet('https://pastebin.com/raw/urvzBY4a'))()
 --Loadstring: loadstring(game:HttpGet('https://raw.githubusercontent.com/Pawstep/scripts/main/DigToChina.lua'))()
 --https://www.roblox.com/games/8506369721/dtc
 --Wait until timer hits 00:00 and you will get to China.
+
 local p=game.Players.LocalPlayer
 local h=plr.Character.HumanoidRootPart
 local c=workspace.ChinaDetector
 local f=firetouchinterest
-
 while true do
    f(h, c, 0)
    task.wait(.1)
