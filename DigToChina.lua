b02e88ade3a14a00661a3631710e611ba51b1e9d
paw <5dkyQHivc7zYxjCmN4qAMH7qtkfPn8@proton.me>
2022-12-10 11:51:43 -0700

    Fixed a local player error.

--- DigToChina.lua
+++ DigToChina.lua
@@ -3,6 +3,7 @@ loadstring(game:HttpGet('https://pastebin.com/raw/urvzBY4a'))()
 --https://www.roblox.com/games/8506369721/dtc
 --Wait until timer hits 00:00 and you will get to China.
 
+
 local p=game.Players.LocalPlayer
 local h=p.Character.HumanoidRootPart
 local c=workspace.ChinaDetector
