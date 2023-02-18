ef323b7a9830c816d4a4b87014a8564f73dc3ad2
xrt <xrtzrr@proton.me>
2023-02-18 16:54:52 -0700

    Update Ro-chanics.lua

--- Ro-chanics.lua
+++ Ro-chanics.lua
@@ -1,5 +1,6 @@
--- Made for Ro-Chanics // https://www.roblox.com/games/2397424135/RC
--- Converted by using GUI to Lua v3.2!
+--> Made for Ro-Chanics // https://www.roblox.com/games/2397424135/RC
+--> Loadstring: loadstring(game:HttpGet("https://raw.githubusercontent.com/xrtzrr/scripts/main/Ro-chanics.lua"))()
+--> Converted by using GUI to Lua v3.2!
 
 local RoChanicsAutoTPtoLabs = Instance.new("ScreenGui")
 local Frame = Instance.new("Frame")
