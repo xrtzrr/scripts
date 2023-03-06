15770819e9eff86c2d8c93ab35dfb99c7a091dd6
xrt <xrtzrr@proton.me>
2023-03-06 12:08:50 -0700

    Update Ro-chanics.lua

--- Ro-chanics.lua
+++ Ro-chanics.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("https://pastebin.com/raw/urvzBY4a"))()
+loadstring(game:HttpGet('https://pastebin.com/raw/tZJF8g3K'))()
 --> Made for Ro-Chanics // https://www.roblox.com/games/2397424135/RC
 --> Loadstring: loadstring(game:HttpGet("https://raw.githubusercontent.com/xrtzrr/scripts/main/Ro-chanics.lua"))()
 --> Converted by using GUI to Lua v3.2!
