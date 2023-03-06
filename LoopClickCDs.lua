cc22c357733d900557a48d6a4149659a2e64e1ca
xrt <xrtzrr@proton.me>
2023-03-06 11:59:25 -0700

    Update LoopClickCDs.lua

--- LoopClickCDs.lua
+++ LoopClickCDs.lua
@@ -1,4 +1,3 @@
-loadstring(game:HttpGet('https://pastebin.com/raw/urvzBY4a'))()
 --To edit wait time (local ttime), check out this pastebin first which has wait time editing along with the loadstring:
 --https://pastebin.com/raw/cZxXthEx
 
