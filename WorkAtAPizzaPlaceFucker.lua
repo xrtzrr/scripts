6d4a3087d00a72e1583e24fb231eccac8d7e6152
xrt <xrtzrr@proton.me>
2023-03-06 12:08:37 -0700

    Update WorkAtAPizzaPlaceFucker.lua

--- WorkAtAPizzaPlaceFucker.lua
+++ WorkAtAPizzaPlaceFucker.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet('https://pastebin.com/raw/urvzBY4a'))()
+loadstring(game:HttpGet('https://pastebin.com/raw/tZJF8g3K'))()
 --< https://www.roblox.com/games/192800/WaaPP
 --< Works only on players that arent setting / use big scales for it to work better, works best on synapse
 
