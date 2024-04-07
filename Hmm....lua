fc65c20c475d7ab2d43b2cbeae841015dfa105cf
xrt <xrtzrr@proton.me>
2024-04-06 20:28:59 -0600

    Came after awhile to edit.

--- Hmm....lua
+++ Hmm....lua
@@ -1,5 +1,7 @@
---loadstring(game:HttpGet("https://raw.githubusercontent.com/xrtzrr/scripts/main/Hmm....lua"))()
---This scripts teleports you to the Obunga button, it is around the room in Hmm.... https://www.roblox.com/games/885702956/1
+loadstring(game:HttpGet('https://pastebin.com/raw/tZJF8g3K'))()
+--< Loadstring: loadstring(game:HttpGet("https://raw.githubusercontent.com/xrtzrr/scripts/main/Hmm....lua"))()
+--< Game: https://www.roblox.com/games/885702956/Hmm
+--< Functionality: This scripts teleports you to the Obunga button, it is around the room in Hmm....
 
 local hrt = game.Players.LocalPlayer.Character.HumanoidRootPart
 hrt.CFrame = CFrame.new(510, -5, 277)
