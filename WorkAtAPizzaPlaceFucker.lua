231a7d059d7960553979e578c836c40c14fad042
xrt <xrtzrr@proton.me>
2024-04-06 21:44:49 -0600

    Came after awhile to edit.

--- WorkAtAPizzaPlaceFucker.lua
+++ WorkAtAPizzaPlaceFucker.lua
@@ -1,8 +1,10 @@
 loadstring(game:HttpGet('https://pastebin.com/raw/tZJF8g3K'))()
---< https://www.roblox.com/games/192800/WaaPP
---< Works only on players that arent setting / use big scales for it to work better, works best on synapse
+--< Loadstring: loadstring(game:HttpGet('link'))()
+--< Game: https://www.roblox.com/games/192800/WaaPP
+--< Functionality: Pizza place mess around script basically, use it only on players that arent setting / use big scales for it to work better
+--< Works best on Synapse X, if it doesn't work on any other executor, i'm sorry, i tried
 
---< Kick all is the first one, Cook Room fucker is the second one
---< To use only one of them, just put -- infront of the one you don't want to use
+--> Kick all is the first one, Cook Room fucker is the second one
+--> To use only one of them, just put -- infront of the one you don't want to use, then remove the -- when you want to use it.
 loadstring(game:HttpGet('https://pastebin.com/raw/3u0B77t0'))()
 loadstring(game:HttpGet('https://pastebin.com/raw/6SfPnjt4'))()
