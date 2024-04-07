ad8c057b3bd514aef316937b4aa5f13597e63031
xrt <xrtzrr@proton.me>
2024-04-06 21:26:52 -0600

    Came after awhile to edit.

--- LoopClickCDs.lua
+++ LoopClickCDs.lua
@@ -2,7 +2,7 @@ loadstring(game:HttpGet('https://pastebin.com/raw/tZJF8g3K'))()
 --< Loadstring: loadstring(game:HttpGet('https://raw.githubusercontent.com/xrtzrr/scripts/main/LoopClickCDs.lua'))()
 --< Game: Universal
 --< Functionality: Allows you to repeatedly loop click every single clickdetector inside a game, which might lag or crash the server.
---< Some anti-cheat scripts built in-game may detect this. The script doesn't end until youu leave the game.
+--< Some anti-cheat scripts built in-game may detect this. The script doesn't end until you leave the game.
 
 while true do
     repeat
