bbd530ede9cbe55d67be9e78ffcc12d13190f2ef
xrt <xrtzrr@proton.me>
2024-09-15 14:17:27 -0600

    [9/15] cleared up code & fixed a few inputs

--- IA_AdminKick.lua
+++ IA_AdminKick.lua
@@ -1,14 +1,18 @@
 loadstring(game:HttpGet('https://pastebin.com/raw/tZJF8g3K'))()
 wait(5)
---[[
+
+
+                                                                                                                                                                                         --[[
 Loadstring: loadstring(game:HttpGet('https://github.com/xrtzrr/scripts/raw/main/IA_AdminKick.lua'))()
 Game: https://www.roblox.com/games/5670218884/IA
 Functionality: Avoid getting banned and avoid in-game admins, basically.
 
 Kicks you out of the game automatically if an item asylum admin/headadmin/developer joins in-game, can also kick you if a roblox QA tester or admin joins.
-The list will be updated if necessary, i'm not going to add contributors or testers, they won't do anything, if anything, just screenshot you and report you, but who cares?
+The list will be updated if necessary, i'm not going to add contributors or testers, they won't do anything, if anything, just screenshot you and report you, but who cares, really
+
 **NOTE**: I've noticed they do bans in a console now. Be aware of that and use alts to avoid this.
---]]
+                                                                                                                                                                                         --]]
+
 
 if not game:IsLoaded() then
     game.Loaded:Wait()
@@ -16,9 +20,9 @@ end
 
 Players = game:GetService"Players"
 Names = {
---  Trial Moderators (i think they got perms.) [2]:
-    "pokemonfan61980", -- New trial moderator
-    "RG_Amber", -- New trial moderator
+--  Trial Moderators [2]:
+    "pokemonfan61980", -- New Trial Moderator
+    "RG_Amber", -- New Trial Moderator
 
 --  Server & Game Moderators [0]:
     
@@ -29,58 +33,58 @@ Names = {
     "TazNova",
     "TheLunaticSome",
     "Rikkanashiii",
-    "Rivrax6", -- Past Trial Moderator (Promoted)
-    "ka_zena", -- Past Trial Moderator (Promoted)
-    "HyoreiNeko", -- Past Trial Moderator (Promoted)
-    "Chill_yz", -- Past Trial Moderator (Promoted)
-    "theYurizone", -- Past Trial Moderator (Promoted)
-    "Candle_Pastries", -- Past Moderator (Promoted)
-    "Scythecel", -- Past Moderator & Trial Moderator (Promoted) Old username: ChlomakiTheWitch
+    "Rivrax6", -- Past Trial Moderator, Promoted to Administrator
+    "ka_zena", -- Past Trial Moderator, Promoted to Administrator
+    "HyoreiNeko", -- Past Trial Moderator, Promoted to Administrator
+    "Chill_yz", -- Past Trial Moderator, Promoted to Administrator
+    "theYurizone", -- Past Trial Moderator, Promoted to Administrator
+    "Candle_Pastries", -- Past Moderator, Promoted to Administrator
+    "Scythecel", -- Trial Moderator & Past Moderator, then Promoted to Admin | Old username: ChlomakiTheWitch
 
 --  Head Administrators [2]:
-    "Emerald_Plasma", -- Past Moderator & Administrator (Promoted)
+    "Emerald_Plasma", -- Past Moderator & Administrator, Promoted to Head Administrator
     "cowoika",
 
---  "Assistant" (unneeded ass role) [3]:
+--  Assistant [3]:
     "fiveplutos",
     "ExpressTrainToHell",
     "Blithwin", -- New assistant
 
 --  Game Developers [4]:
-    "JeanRBLX", -- Holder for group.
-    "LEBWEE",  -- Past Music Developer
-    "Siterior", -- Past Administrator promoted to Game Developer
+    "JeanRBLX", -- Group Holder
+    "LEBWEE",  -- Past Music Developer, Promoted to Game Developer
+    "Siterior", -- Past Administrator, Promoted to Game Developer
     "PortABoi",
 
 -- Adding previous admins here because they probably have relations with staff and can probably report you to the current game moderators.
 -- Other Staff [24]:
     "dilbertron2", -- Past Moderator, Promoted to Administrator, then left JPX for unknown reasons.
-    "JonathanFoxx", -- Administrator, Demoted to Tester, lol
-    "Lovely_Sammer", -- Administrator, Demoted to Tester, lol
-    "Enestarhan2018", --  Former Moderator, Demoted to Respected LOL.
-    "Xerroz", -- Left JPX Studios (RIP, he was a good guy and one of the main devs.)
-    "XDavodioX", -- Got demoted from Assistant to Contributor, lol
-    "BeezelduD", -- Got demoted from Assistant to Contributor, lol
-    "Tidyen", -- Got demoted from Assistant to Contributor, lol
-    "Weko", -- Past Head Administrator demoted to Member, hah!
-    "TrixisDev", -- Past Administrator and Pedophile (Rumored XD)
-    "Pixelmen360", -- Past Head Administrator (no idea why the fuck he got fired..)
-    "TimidNomf", -- Past Administrator, Demoted to Member, fuck off you kemono-head wearer. (probably a child groomer ngl.)
+    "JonathanFoxx", -- Administrator, Demoted to Tester
+    "Lovely_Sammer", -- Administrator, Demoted to Tester
+    "Enestarhan2018", --  Former Moderator, Demoted to Respected
+    "Xerroz", -- Left JPX Studios | RIP, he was a good guy and one of the main devs.
+    "XDavodioX", -- Got demoted from Assistant to Contributor
+    "BeezelduD", -- Got demoted from Assistant to Contributor
+    "Tidyen", -- Got demoted from Assistant to Contributor
+    "Weko", -- Past Head Administrator demoted to Member
+    "TrixisDev", -- Past Administrator | and also a Pedophile (Allegedly!)
+    "Pixelmen360", -- Past Head Administrator
+    "TimidNomf", -- Past Administrator, Demoted to Member | Kemono-head wearer (ew.)
     "Lanternsaurus", -- Past Moderator
     "D1s_c", -- Past Administrator
-    "raaymaark", -- Past Administrator, Demoted to Trial Moderator, then Demoted to Member. RIP. Old username: Iei5h
+    "raaymaark", -- Past Administrator, Demoted to Trial Moderator, then Demoted to Member. RIP. | Old username: Iei5h
     "SniffE_Boi", -- Past Administrator
     "Nanachiiky", -- Past Administrator
     "FormalNoob", -- Past Administrator
     "Acorlan", -- Past Administrator
     "dilbertron2" -- Past Administrator
-    "loosefreak", -- Past Moderator, haha
-    "EquusQuagga", -- Past Moderator, haha
-    "DJSpaceInvaderz", -- Past Moderator, haha
+    "loosefreak", -- Past Moderator
+    "EquusQuagga", -- Past Moderator
+    "DJSpaceInvaderz", -- Past Moderator
     "NFKitsune" -- Past Administrator
 
--- Total staff team (including ex-staff): 47
--- Total staff team (not including ex-staff): 23
+-- Total Staff team (including ex-staff): 47
+-- Total Staff team (not including ex-staff): 23
 
 
 
